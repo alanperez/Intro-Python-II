13ed6eb047db22adf49ba1e27bf8f0fbe86b09ce
Alan Perez <alanp9six@gmail.com>
2019-10-03 16:40:18 -0500

    player attr

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
+
+
+# p1 = Player("Alan", "Entrance")
+
+# print(f'Name: {p1.name}, Current Room: {p1.currentRoom}')
