d4ed2be7525b0001188b911a8250293e2e59379d
Alan Perez <alanp9six@gmail.com>
2019-10-03 16:41:03 -0500

    room stup ;

--- src/room.py
+++ src/room.py
@@ -2,3 +2,14 @@
 # description attributes.
 
 # initial commit
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+        self.items = []
